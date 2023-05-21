7deed64a9749268b41b28980a626b41b2f0996bd
StartDeveloperKim <ktw550111@naver.com>
2023-05-21 14:54:17 +0900

    Feat. 스타트 페이지 로직 완성

---
+++ app/src/main/java/com/example/aiapplication/layout/StartActivity.java
@@ -0,0 +1,33 @@
+package com.example.aiapplication.layout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.aiapplication.MainActivity;
+import com.example.aiapplication.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+}
