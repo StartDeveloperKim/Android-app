63f7a4afb9c0243870f144b82433dbfdb9972c33
StartDeveloperKim <ktw550111@naver.com>
2023-05-21 16:08:06 +0900

    Fix. Photo 클래스에 관련 메서드 넣음 -> 리팩토링

--- app/src/main/java/com/example/aiapplication/MainActivity.java
+++ app/src/main/java/com/example/aiapplication/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.aiapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -22,6 +19,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import com.example.aiapplication.camera.Photo;
 import com.example.aiapplication.layout.MyDataActivity;
 import com.example.aiapplication.layout.SettingActivity;
 import com.gun0912.tedpermission.PermissionListener;
@@ -29,24 +27,22 @@ import com.gun0912.tedpermission.TedPermission;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 672;
 
-    private String imageFilePath;
-    private Uri photoUri;
     private ActivityResultLauncher<Intent> resultLauncher;
+    private Photo photo;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        photo = new Photo(); // 카메라 인스턴스 생성
 
         //권한 체크
         TedPermission.with(getApplicationContext())
@@ -63,89 +59,16 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if (result.getResultCode() == Activity.RESULT_OK) {
-                            afterTakePicture();
+                            Bitmap bitmap = photo.afterTakePicture();
+                            ImageView imageView = findViewById(R.id.iv_result);
+                            imageView.setImageBitmap(bitmap);
                         }
                     }
                 }
         );
 
-        //촬영 버튼 눌렀을 때
-        findViewById(R.id.btn_capture).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    File photoFile = null;
-                    try {
-                        photoFile = createImageFile();
-                    } catch (IOException e) {
-
-                    }
-
-                    if (photoFile != null) {
-                        photoUri = FileProvider.getUriForFile(getApplicationContext(), getPackageName(), photoFile);
-                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-                        resultLauncher.launch(intent);
-                    }
-                }
-            }
-        });
-
     }
 
-    private File createImageFile() throws IOException{
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "TEST_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,
-                ".jpg",
-                storageDir
-        );
-
-        imageFilePath = image.getAbsolutePath();
-        return image;
-    }
-
-    private void afterTakePicture(){
-        Bitmap bitmap = BitmapFactory.decodeFile(imageFilePath);
-        ExifInterface exif = null;
-
-        try {
-            exif = new ExifInterface(imageFilePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        int exifOrientation;
-        int exifDegree;
-
-        if (exif != null) {
-            exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-            exifDegree = exifOrientationToDegrees(exifOrientation);
-        } else {
-            exifDegree = 0;
-        }
-        ((ImageView) findViewById(R.id.iv_result)).setImageBitmap(rotate(bitmap, exifDegree));
-    }
-
-    private Bitmap rotate(Bitmap bitmap, float degree) {
-        Matrix matrix = new Matrix();
-        matrix.postRotate(degree);
-        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-    }
-
-    // 이미지 회전을 위한 메서드
-    private int exifOrientationToDegrees(int exifOrientation) {
-        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
-            return 90;
-        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
-            return 180;
-        }else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
-            return 270;
-        }
-        return 0;
-    }
 
     PermissionListener permissionListener = new PermissionListener() {
         @Override
@@ -159,6 +82,25 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
+    public void clickCameraButton(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = photo.createImageFile(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES));
+            } catch (IOException e) {
+
+            }
+
+            if (photoFile != null) {
+                Uri photoUri = FileProvider.getUriForFile(getApplicationContext(), getPackageName(), photoFile);
+                photo.setPhotoUri(photoUri);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                resultLauncher.launch(intent);
+            }
+        }
+    }
 
     public void clickMyDataButton(View view) {
         Intent intent = new Intent(getApplicationContext(), MyDataActivity.class);
